c8965b2c1bd4a72c0a259d453a5d080b97e7fada
zhd <1924758151@qq.com>
2020-05-15 11:36:14 +0800

    背包食物信息

---
+++ game-base/src/main/java/com/dkm/pets/entity/dto/BackPackFood.java
@@ -0,0 +1,14 @@
+package com.dkm.pets.entity.dto;
+
+/**
+ * @program: game_project
+ * @description: 背包食物信息
+ * @author: zhd
+ * @create: 2020-05-15 11:30
+ **/
+public class BackPackFood {
+    private String foodName;
+    private String foodUrl;
+    private Integer foodGold;
+    private Integer foodNumber;
+}
