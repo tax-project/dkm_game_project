d7cada1a7661b00ca9ded3a22b1c01fd87523ed8
HuangJie <huangjie007x@gmail>
2020-05-15 11:37:10 +0800

    huangjie

--- game-base/src/main/java/com/dkm/backpack/controller/BackpackController.java
+++ game-base/src/main/java/com/dkm/backpack/controller/BackpackController.java
@@ -1,10 +1,14 @@
 package com.dkm.backpack.controller;
 
+import com.dkm.backpack.entity.bo.AddBackpackItemBO;
+import com.dkm.backpack.service.IBackpackService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/backpack")
 public class BackpackController {
 
+    @Autowired
+    private IBackpackService backpackService;
+
     @PostMapping("/add/backpack/item")
     @ApiOperation(value = "添加背包物品",notes = "添加背包物品",produces = "application/json")
     @ApiImplicitParams({
+            @ApiImplicitParam(name = "token",value = "用户token",dataType = "String",paramType = "header",required = true),
             @ApiImplicitParam(name = "itemId",value = "物品的ID",dataType = "Long",paramType = "body",required = true),
-            @ApiImplicitParam(name = "itemTableName",value = "物品所在表名称",dataType = "String",required = true,paramType = "body")
+            @ApiImplicitParam(name = "itemTableNumber",value = "物品所在表的编号，食物为1，装备为2，衣服为3",dataType = "Integer",required = true,paramType = "body"),
+            @ApiImplicitParam(name = "itemNumber",value = "物品数量",dataType = "Integer",required = true,paramType = "body")
     })
-    public void addBackpackItem(){
-        
+    public void addBackpackItem(@RequestBody AddBackpackItemBO backpackItemBO){
+        backpackService.addBackpackItem(backpackItemBO);
     }
 
 }
---
+++ game-base/src/main/java/com/dkm/backpack/entity/bo/AddBackpackItemBO.java
@@ -0,0 +1,24 @@
+package com.dkm.backpack.entity.bo;
+
+import lombok.Data;
+
+/**
+ * @Author: HuangJie
+ * @Date: 2020/5/15 10:39
+ * @Version: 1.0V
+ */
+@Data
+public class AddBackpackItemBO {
+    /**
+     * 物品ID
+     */
+    private Long itemId;
+    /**
+     * 物品所在表编号
+     */
+    private Integer itemTableNumber;
+    /**
+     * 物品数量
+     */
+    private Integer itemNumber;
+}
