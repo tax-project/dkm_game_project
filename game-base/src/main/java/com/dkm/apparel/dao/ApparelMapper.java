daffe17227e5533ebf11d7fcee261eae45d8daee
zhd <1924758151@qq.com>
2020-05-16 21:47:09 +0800

    服饰

---
+++ game-base/src/main/java/com/dkm/apparel/dao/ApparelMapper.java
@@ -0,0 +1,14 @@
+package com.dkm.apparel.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dkm.apparel.entity.ApparelEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @description: 服饰
+ * @author: zhd
+ * @create: 2020-05-15 14:03
+ **/
+@Repository
+public interface ApparelMapper extends BaseMapper<ApparelEntity> {
+}
---
+++ game-base/src/main/java/com/dkm/apparel/entity/ApparelEntity.java
@@ -0,0 +1,34 @@
+package com.dkm.apparel.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @program: game_project
+ * @description: 服饰
+ * @author: zhd
+ * @create: 2020-05-15 13:58
+ **/
+@Data
+@TableName("tb_apparel_detail")
+public class ApparelEntity {
+    @TableId
+    private Long apparelId;
+    /**
+     * 地址
+     */
+    private String apparelUrl;
+    /**
+     * 名称
+     */
+    private String apparelName;
+    /**
+     * 类型
+     */
+    private Integer apparelType;
+    /**
+     * 金币
+     */
+    private Integer apparelGold;
+}
