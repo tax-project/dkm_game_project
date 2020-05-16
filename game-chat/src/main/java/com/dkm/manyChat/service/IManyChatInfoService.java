4a928d431f58ce22434be3e8b216eeb79b6bc905
vincent <vincent@eqlee.com>
2020-05-16 11:58:24 +0800

    'qf'

--- game-chat/src/main/java/com/dkm/manyChat/service/IManyChatInfoService.java
+++ game-chat/src/main/java/com/dkm/manyChat/service/IManyChatInfoService.java
@@ -1,9 +1,19 @@
 package com.dkm.manyChat.service;
 
+import com.dkm.manyChat.entity.vo.ManyChatInfoVo;
+
+import java.util.List;
+
 /**
  * @author qf
  * @date 2020/5/15
  * @vesion 1.0
  **/
 public interface IManyChatInfoService {
+
+   /**
+    *  建立群聊的具体群员
+    * @param list 成员信息
+    */
+   void insertAllUser (List<ManyChatInfoVo> list);
 }
--- game-chat/src/main/java/com/dkm/manyChat/service/impl/ManyChatInfoServiceImpl.java
+++ game-chat/src/main/java/com/dkm/manyChat/service/impl/ManyChatInfoServiceImpl.java
@@ -3,10 +3,13 @@ package com.dkm.manyChat.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dkm.manyChat.dao.ManyChatInfoMapper;
 import com.dkm.manyChat.entity.ManyChatInfo;
+import com.dkm.manyChat.entity.vo.ManyChatInfoVo;
 import com.dkm.manyChat.service.IManyChatInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author qf
@@ -20,4 +23,13 @@ public class ManyChatInfoServiceImpl extends ServiceImpl<ManyChatInfoMapper, Man
 
 
 
+   /**
+    *  建立群聊的具体群员
+    * @param list 成员信息
+    */
+   @Override
+   public void insertAllUser(List<ManyChatInfoVo> list) {
+
+
+   }
 }
