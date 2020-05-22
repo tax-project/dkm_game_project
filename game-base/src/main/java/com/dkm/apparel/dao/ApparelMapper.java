da37d700f2847d679b7e7ba316e3f7ee216c4bd9
zhd <1924758151@qq.com>
2020-05-22 11:00:02 +0800

    服饰

--- game-base/src/main/java/com/dkm/apparel/dao/ApparelMapper.java
+++ game-base/src/main/java/com/dkm/apparel/dao/ApparelMapper.java
@@ -23,4 +23,5 @@ public interface ApparelMapper extends BaseMapper<ApparelEntity> {
      */
     @Select("SELECT * FROM (SELECT apparel_detail_id FROM tb_apparel_user WHERE user_id = #{userId}) au left JOIN tb_apparel_detail ad on au.apparel_detail_id=ad.apparel_id")
     List<ApparelEntity> getUserApparel(@Param("userId")Long userId);
+
 }
