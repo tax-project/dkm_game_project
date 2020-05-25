93f8cc22f6d864f936b0131846ec2f71581adc34
zy <1402407104@qq.com>
2020-05-25 13:48:43 +0800

    提交了用户的当前体力和总体力

--- game-base/src/main/java/com/dkm/personalCenter/controller/PersonalCenterController.java
+++ game-base/src/main/java/com/dkm/personalCenter/controller/PersonalCenterController.java
@@ -2,6 +2,7 @@ package com.dkm.personalCenter.controller;
 
 import com.dkm.feign.ResourceFeignClient;
 import com.dkm.feign.fallback.ResourceFeignClientFallback;
+import com.dkm.feign.fallback.UserFeignClientFallback;
 import com.dkm.jwt.contain.LocalUser;
 import com.dkm.jwt.islogin.CheckToken;
 import com.dkm.personalCenter.domain.vo.TbEquipmentKnapsackVo;
@@ -32,9 +33,10 @@ public class PersonalCenterController {
     LocalUser localUser;
     @Autowired
     ResourceFeignClient resourceFeignClient;
-
+    @Autowired
+    UserFeignClientFallback userFeignClientFallback;
     @ApiOperation(value = "个人中心的查询接口",notes = "equipment 为装备的数据 blackHouse 为黑屋的用户信息对象 Seed为查询用户解锁的种子" +
-            "  queryMySkill 查询我的技能   查询跟班产出的产物 AttendantGoods")
+            "  queryMySkill 查询我的技能  AttendantGoods 查询跟班产出的产物  queryUser 为用户总体力和当前体力")
     @GetMapping("/selectAll")
     @CrossOrigin
     //@CheckToken
@@ -50,6 +52,8 @@ public class PersonalCenterController {
         map.put("queryMySkill",resourceFeignClient.queryMySkill());
         //查询跟班产出的产物
         map.put("AttendantGoods",resourceFeignClient.queryJoinOutPutGoods());
+        //查询出用户的总体力和当前体力
+        map.put("queryUser",userFeignClientFallback.queryUser(localUser.getUser().getId()));
         return map;
     }
 }
