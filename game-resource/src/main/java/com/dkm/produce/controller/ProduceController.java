11b126160efbeb6a857ae0885ecb4818d3e2b587
zy <1402407104@qq.com>
2020-05-28 11:49:05 +0800

    修改feign方法地址31

--- game-resource/src/main/java/com/dkm/produce/controller/ProduceController.java
+++ game-resource/src/main/java/com/dkm/produce/controller/ProduceController.java
@@ -44,7 +44,7 @@ public class ProduceController {
     @ApiOperation(value = "根据用户id查询跟班和跟班产生的物品", notes = "根据用户id查询跟班和跟班产生的物品")
     @GetMapping("/queryJoinOutPutGoods")
     @CrossOrigin
-    public List<AttendantGoods> queryJoinOutPutGoods(@RequestParam("attendantId") Long userId){
+    public List<AttendantGoods> queryJoinOutPutGoods(@RequestParam(value = "userId") Long userId){
         return produceService.queryJoinOutPutGoods(userId);
     }
 
