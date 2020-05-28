01cb19d126529075ec949d2c05e43810d820049c
vincent <vincent@eqlee.com>
2020-05-28 14:10:24 +0800

    'qf'

--- game-base/src/main/java/com/dkm/feign/fallback/ResourceFeignClientFallback.java
+++ game-base/src/main/java/com/dkm/feign/fallback/ResourceFeignClientFallback.java
@@ -6,6 +6,7 @@ import com.dkm.entity.bo.SkillBo;
 import com.dkm.feign.ResourceFeignClient;
 import com.dkm.feign.entity.AttendantGoods;
 import com.dkm.feign.entity.MySkillVo;
+import com.dkm.feign.entity.Skill;
 import com.dkm.housekeeper.entity.vo.TbEquipmentVo;
 import com.dkm.personalCenter.domain.Seed;
 import com.dkm.personalCenter.domain.vo.TbBlackHouseVo;
@@ -25,45 +26,47 @@ import java.util.List;
 public class ResourceFeignClientFallback implements ResourceFeignClient {
     @Override
     public Result updateIsva(Long tekId, Integer foodNumber) {
-        return Result.fail(CodeType.SERVICE_ERROR);
+        return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
     }
 
     @Override
     public Result<List<TbEquipmentKnapsackVo>> selectUserIdAndFoodId(Long userId) {
-        return Result.fail(CodeType.DATABASE_ERROR);
-    }
-
-    @Override
-    public Result<List<com.dkm.personalCenter.domain.vo.TbEquipmentKnapsackVo>> userCenter() {
         return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
     }
 
-    @Override
-    public Result<TbBlackHouseVo> selectIsBlackTwo() {
-        return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
-    }
+   @Override
+   public Result<List<com.dkm.personalCenter.domain.vo.TbEquipmentKnapsackVo>> userCenter(Long userId) {
+      return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
+   }
 
-    @Override
-    public  Result<List<Seed>> queryAreUnlocked() {
-        return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
-    }
+   @Override
+   public Result<TbBlackHouseVo> selectIsBlackTwo(Long userId) {
+      return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
+   }
 
-    @Override
+   @Override
+   public Result<List<Seed>> queryAreUnlocked(Long userId) {
+      return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
+   }
+
+
+   @Override
     public Result<List<TbEquipmentVo>> selectByBoxIdTwo(List<Long> boxId) {
-        return Result.fail(CodeType.DATABASE_ERROR);
-    }
-
-    @Override
-    public Result<List<MySkillVo>> queryMySkill() {
         return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
     }
 
-    @Override
-    public Result<List<AttendantGoods>> queryJoinOutPutGoods() {
-        return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
-    }
+   @Override
+   public Result<List<Skill>> listAllSkill() {
+      return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
+   }
 
-    @Override
+   @Override
+   public Result<List<AttendantGoods>> queryJoinOutPutGoods(Long userId) {
+      return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
+   }
+
+
+   @Override
     public Result<List<SkillBo>> queryAllSkillByUserId(Long userId) {
         return Result.fail(CodeType.FEIGN_CONNECT_ERROR);
     }
