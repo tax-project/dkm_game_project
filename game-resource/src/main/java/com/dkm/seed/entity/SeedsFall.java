e363cf875ed5acdab028fb7f4f750dbbbd125810
zy <1402407104@qq.com>
2020-06-08 17:14:11 +0800

    mq

---
+++ game-resource/src/main/java/com/dkm/seed/entity/SeedsFall.java
@@ -0,0 +1,34 @@
+package com.dkm.seed.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author 刘梦祺
+ * @PROJECT_NAME: game_project
+ * @DESCRIPTION:
+ * @DATE: 2020/6/8 11:36
+ */
+@Data
+@TableName("tb_seeds_fall")
+public class SeedsFall extends Model<SeedsFall> {
+    /**
+     *种子详情id
+     */
+    private Long id;
+    /**
+     * 种子id
+     */
+    private Long seedId;
+    /**
+     * 掉落的金币
+     */
+    private Integer dropCoins;
+    /**
+     * 掉落的红包
+     */
+    private Double dropRedEnvelope;
+}
