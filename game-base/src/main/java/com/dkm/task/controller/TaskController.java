ab79db0546b17f0afcc9121f6e36a722810c1821
zhd <1924758151@qq.com>
2020-06-08 16:32:36 +0800

    任务模块（待完善）

---
+++ game-base/src/main/java/com/dkm/task/controller/TaskController.java
@@ -0,0 +1,53 @@
+package com.dkm.task.controller;
+
+import com.dkm.jwt.contain.LocalUser;
+import com.dkm.jwt.islogin.CheckToken;
+import com.dkm.task.entity.vo.TaskUserDetailVo;
+import com.dkm.task.service.TaskService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @program: game_project
+ * @description: 任务接口
+ * @author: zhd
+ * @create: 2020-06-08 10:27
+ **/
+@Api(tags = "任务API")
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Resource
+    private LocalUser localUser;
+
+    @Resource
+    private TaskService taskService;
+
+    @ApiOperation("获取任务列表")
+    @ApiImplicitParam(name = "type",value = "0 成长 1 日常 2成就",paramType = "path",dataType = "Integer",required = true)
+    @CrossOrigin
+    @GetMapping("/getUserTask")
+    @CheckToken
+    public List<TaskUserDetailVo> getUserTask(Integer type){
+        return taskService.selectUserTask(localUser.getUser().getId(),type);
+    }
+
+    @ApiOperation("领取任务奖励")
+    @ApiImplicitParam(name = "taskId",value = "任务id",paramType = "path",dataType = "Long",required = true)
+    @CrossOrigin
+    @GetMapping("/getUserTask")
+    @CheckToken
+    public void getUserTask(Long taskId){
+        taskService.getTaskReward(localUser.getUser().getId(),taskId);
+    }
+
+}
