5336235a2d6bdf6c466b581a073a1c3fa8a77e69
HuangJie <huangjie007x@gmail>
2020-06-09 10:33:26 +0800

    huangjie

--- game-base/src/main/java/com/dkm/listener/RabbitMqListener.java
+++ game-base/src/main/java/com/dkm/listener/RabbitMqListener.java
@@ -1,16 +1,7 @@
 package com.dkm.listener;
 
-import com.alibaba.fastjson.JSONObject;
-import com.dkm.entity.websocket.MsgInfo;
-import com.rabbitmq.client.Channel;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author qf
