ba41f9dc9ccbcaa6a32623dce1a20c8f64f1edbf
vincent <vincent@eqlee.com>
2020-06-12 14:38:10 +0800

    'qf'

--- game-chat/src/main/java/com/dkm/feign/fallback/FileFeignClientFallback.java
+++ game-chat/src/main/java/com/dkm/feign/fallback/FileFeignClientFallback.java
@@ -4,12 +4,14 @@ import com.dkm.constanct.CodeType;
 import com.dkm.data.Result;
 import com.dkm.entity.vo.FileVo;
 import com.dkm.feign.FileFeignClient;
+import org.springframework.stereotype.Component;
 
 /**
  * @author qf
  * @date 2020/6/11
  * @vesion 1.0
  **/
+@Component
 public class FileFeignClientFallback implements FileFeignClient {
 
 
