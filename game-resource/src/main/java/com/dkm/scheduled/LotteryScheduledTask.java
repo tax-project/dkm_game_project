5e7b66e623e6dc740f7a2619b8eab94f2ab289af
Noob <ExplodingFKL@gmail.com>
2020-06-20 14:54:49 +0800

    I don't know what has been updated.

---
+++ game-resource/src/main/java/com/dkm/scheduled/LotteryScheduledTask.java
@@ -0,0 +1,15 @@
+package com.dkm.scheduled;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author fkl
+ *
+ */
+@EnableScheduling
+@Configuration
+public class LotteryScheduledTask {
+
+}
