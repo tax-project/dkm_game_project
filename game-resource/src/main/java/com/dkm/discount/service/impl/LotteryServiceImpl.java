5e9667a093b98a586473286db92ee34a9b1a7b1a
Noob <ExplodingFKL@gmail.com>
2020-06-20 17:24:56 +0800

    I don't know what has been updated.

---
+++ game-resource/src/main/java/com/dkm/discount/service/impl/LotteryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.dkm.discount.service.impl;
+
+import com.dkm.discount.entity.vo.LotteryInfoVo;
+import com.dkm.discount.service.ILotteryService;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author fkl
+ */
+@Service
+public class LotteryServiceImpl implements ILotteryService {
+
+    @Override
+    public LotteryInfoVo getAllLotteries(String userId) {
+        val lotteryInfoVo = new LotteryInfoVo();
+
+//        lotteryInfoVo.setNextRefreshDate();
+        return lotteryInfoVo;
+    }
+}
