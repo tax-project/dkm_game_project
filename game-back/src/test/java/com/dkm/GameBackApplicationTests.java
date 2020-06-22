f55dce43729258258a7d0cf01cc6431be97c093a
vincent <vincent@eqlee.com>
2020-06-22 11:20:32 +0800

    '...'

---
+++ game-back/src/test/java/com/dkm/GameBackApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dkm;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBackApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
