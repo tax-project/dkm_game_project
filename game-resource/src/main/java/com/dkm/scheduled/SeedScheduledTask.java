bdfe4ca8c3adb07246bae910592573a6955d26f7
vincent <vincent@eqlee.com>
2020-06-29 16:27:07 +0800

    'qf'

---
+++ game-resource/src/main/java/com/dkm/scheduled/SeedScheduledTask.java
@@ -0,0 +1,27 @@
+package com.dkm.scheduled;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * 种子掉落  10S一次
+ * @author qf
+ * @date 2020/6/29
+ * @vesion 1.0
+ **/
+@Slf4j
+@Component
+public class SeedScheduledTask {
+
+
+
+   @Scheduled(cron = "0/10 * * * * ?")
+   public void toDeleteApply () {
+
+
+
+   }
+}
