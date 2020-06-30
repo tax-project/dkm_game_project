f0cd72de2759b1f27e2168b0d1440992d60f114e
zy <1402407104@qq.com>
2020-06-30 15:36:30 +0800

    mq

--- game-resource/src/main/java/com/dkm/seed/vilidata/RandomUtils.java
+++ game-resource/src/main/java/com/dkm/seed/vilidata/RandomUtils.java
@@ -94,7 +94,7 @@ public class RandomUtils {
 
         Random random=new Random();
         int i = random.nextInt(100) + 1;
-        if(i<=2){
+        if(i<=3){
             return 1;
         }
         return 0;
