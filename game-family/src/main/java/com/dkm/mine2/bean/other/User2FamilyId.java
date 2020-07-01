c39b23942895a7227f6b323c016d863a2dfdb6ac
Noob <ExplodingFKL@gmail.com>
2020-07-01 17:03:35 +0800

    重构家族矿区.

---
+++ game-family/src/main/java/com/dkm/mine2/bean/other/User2FamilyId.java
@@ -0,0 +1,11 @@
+package com.dkm.mine2.bean.other;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class User2FamilyId {
+    private Long userId;
+    private Long familyId;
+}
