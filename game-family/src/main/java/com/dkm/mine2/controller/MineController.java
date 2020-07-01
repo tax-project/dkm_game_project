b7a61aba8a1e3859474f2a7b9a852a2f8d91d92c
Noob <ExplodingFKL@gmail.com>
2020-07-01 16:23:49 +0800

    修复了家族金矿对战的错误

---
+++ game-family/src/main/java/com/dkm/mine2/controller/MineController.java
@@ -0,0 +1,16 @@
+package com.dkm.mine2.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author 符凯龙
+ */
+@Api(tags = "新的家族矿区 API")
+@RequestMapping("/mine/")
+@RestController
+public class MineController {
+
+}
