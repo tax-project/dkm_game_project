e711c1b109d2f28de7ade60f7bf29bcfe3d79383
zy <1402407104@qq.com>
2020-07-03 10:25:21 +0800

    掉落金币接口随机数 处理4

--- game-resource/src/main/java/com/dkm/plunder/service/impl/OpponentServiceImpl.java
+++ game-resource/src/main/java/com/dkm/plunder/service/impl/OpponentServiceImpl.java
@@ -6,7 +6,10 @@ import com.dkm.plunder.dao.OpponentMapper;
 import com.dkm.plunder.entity.Opponent;
 import com.dkm.plunder.entity.vo.OpponentVo;
 import com.dkm.plunder.service.IOpponentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
