4602f8206fb6678f9cb5096190455ce62f3ac553
vincent <vincent@eqlee.com>
2020-07-09 16:44:20 +0800

    'qf'

--- game-base/src/main/java/com/dkm/listener/RabbitMqListener.java
+++ game-base/src/main/java/com/dkm/listener/RabbitMqListener.java
@@ -11,27 +11,4 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RabbitMqListener {
 
-
-//   @RabbitHandler
-//   public void getMsg (@Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag, String msgInfo, Channel channel) {
-//      log.info("通讯平台收到服务器的消息:" +msgInfo);
-//
-//      //判断是不是群聊消息
-//      MsgInfo info = null;
-//      try {
-//         info = JSONObject.parseObject(msgInfo, MsgInfo.class);
-//      } catch (Exception e) {
-//         log.info("rabbitMq接收业务平台的消息转换有误...");
-//         e.printStackTrace();
-//      }
-//
-//
-//
-//      //确认消息
-//      try {
-//         channel.basicAck(deliveryTag,true);
-//      } catch (IOException e) {
-//         e.printStackTrace();
-//      }
-//   }
 }
