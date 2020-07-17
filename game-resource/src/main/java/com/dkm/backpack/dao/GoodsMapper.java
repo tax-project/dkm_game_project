c2174151b17036d18a2a746a61e3fe38399b9381
zhd <1924758151@qq.com>
2020-07-17 15:37:07 +0800

    zhd

---
+++ game-resource/src/main/java/com/dkm/backpack/dao/GoodsMapper.java
@@ -0,0 +1,18 @@
+package com.dkm.backpack.dao;
+
+import com.dkm.IBaseMapper.IBaseMapper;
+import com.dkm.backpack.entity.GoodsEntity;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * @description:
+ * @author: zhd
+ * @create: 2020-06-23 10:35
+ **/
+@Repository
+public interface GoodsMapper extends IBaseMapper<GoodsEntity> {
+
+
+
+}
