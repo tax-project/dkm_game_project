437e5c8198ca89dac591de03d19cbde39565e688
Noob <ExplodingFKL@gmail.com>
2020-07-29 13:47:01 +0800

    30元充值

--- game-resource/src/main/java/com/dkm/mallEvents/service/impl/CancerServiceImpl.java
+++ game-resource/src/main/java/com/dkm/mallEvents/service/impl/CancerServiceImpl.java
@@ -1,7 +1,12 @@
 package com.dkm.mallEvents.service.impl;
 
+import com.dkm.backpack.entity.bo.AddGoodsInfo;
+import com.dkm.backpack.service.IBackpackService;
+import com.dkm.constanct.CodeType;
+import com.dkm.exception.ApplicationException;
 import com.dkm.mallEvents.dao.CancerDao;
 import com.dkm.mallEvents.entities.vo.GoodsInfoVo;
+import com.dkm.mallEvents.entities.vo.SingleHistoryUserVo;
 import com.dkm.mallEvents.entities.vo.SingleTopUpVo;
 import com.dkm.mallEvents.service.ICancerService;
 import org.springframework.stereotype.Service;
@@ -12,7 +17,8 @@ import java.util.*;
 @Service
 public class CancerServiceImpl implements ICancerService {
 
-
+    @Resource
+    private IBackpackService backpackService;
 
     @Resource
     private CancerDao cancerDao;
@@ -26,9 +32,30 @@ public class CancerServiceImpl implements ICancerService {
         return singleTopUpVos;
     }
 
+    /**
+     *
+     *  注意啦，这个没有判断是否充值完成的，因为暂时不存在充值模块
+     *
+     */
     @Override
     public Boolean getSingleTopUpInfoCheck(Long userId, Integer id) {
-        List<GoodsInfoVo> goods =cancerDao.selectById(id);
-        return null;
+
+        List<GoodsInfoVo> goods = cancerDao.selectById(id);
+        if (goods == null || goods.size() == 0) {
+            throw new ApplicationException(CodeType.PARAMETER_ERROR, "未找到此数据");
+        }
+        SingleHistoryUserVo historyUserVo = cancerDao.selectHistoryByUser(userId,id);
+        if (historyUserVo != null){
+            throw new ApplicationException(CodeType.RESOURCES_EXISTING,"你已经领取过啦.");
+        }
+        for (GoodsInfoVo good : goods) {
+            AddGoodsInfo addGoodsInfo = new AddGoodsInfo();
+            addGoodsInfo.setUserId(userId);
+            addGoodsInfo.setGoodId(good.getGoodsId());
+            addGoodsInfo.setNumber(good.getSize());
+            backpackService.addBackpackGoods(addGoodsInfo);
+        }
+        cancerDao.addUser(userId, id);
+        return true;
     }
 }
